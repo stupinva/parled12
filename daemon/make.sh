0bc9a5a45683eb2b0d66124794ef3500ce8ae43c
Vladimir Stupin <vladimir@stupin.su>
2021-08-01 02:46:48 +0500

    В скрипт сборки добавлены опции для удаления из исполняемого файла неиспользуемых функций и данных

--- daemon/make.sh
+++ daemon/make.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-gcc -std=c99 -Wpedantic -Wall -Wextra -D_DEFAULT_SOURCE -o parled12 daemon.c parport.c parports.c evloop.c client.c server.c slave.c config.c master.c main.c
-gcc -std=c99 -Wpedantic -Wall -Wextra -D_DEFAULT_SOURCE -DLITE -o parled12-lite daemon.c parport.c parports.c evloop.c client.c server.c slave.c config.c main.c
-#gcc -std=c99 -Wpedantic -Wall -Wextra -o parled12 parport.c parports.c
+gcc -std=c99 -Wpedantic -Wall -Wextra -D_DEFAULT_SOURCE -fdata-sections -ffunction-sections -Wl,--gc-sections -Wl,--print-gc-sections -Wl,-s -o parled12 daemon.c parport.c parports.c evloop.c client.c server.c slave.c config.c master.c main.c
+gcc -std=c99 -Wpedantic -Wall -Wextra -D_DEFAULT_SOURCE -DLITE -fdata-sections -ffunction-sections -Wl,--gc-sections -Wl,--print-gc-sections -Wl,-s -o parled12-lite daemon.c parport.c parports.c evloop.c client.c server.c slave.c config.c main.c
