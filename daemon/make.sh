d96b7560f73bfe0bdd8d290ac73d7a7e98e96a9d
Vladimir Stupin <vladimir@stupin.su>
2023-04-03 11:36:16 +0500

    Реализована сборка с помощью autotools
    Для сборки нужно выполнить следующую последовательность команд:
    aclocal
    automake --add-missing
    autoconf
    ./configure
    make
    
    Использованные материалы:
    [Использование GNU Autotools. Создание своего скрипта configure и файла
    Makefile](https://help.ubuntu.ru/wiki/using_gnu_autotools)

--- daemon/make.sh
+++ daemon/make.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 gcc -std=c99 -Wpedantic -Wall -Wextra -D_DEFAULT_SOURCE -fdata-sections -ffunction-sections -Wl,--gc-sections -Wl,--print-gc-sections -Wl,-s -o parled12 daemon.c parport.c parports.c evloop.c client.c server.c slave.c config.c master.c main.c
-gcc -std=c99 -Wpedantic -Wall -Wextra -D_DEFAULT_SOURCE -DLITE -fdata-sections -ffunction-sections -Wl,--gc-sections -Wl,--print-gc-sections -Wl,-s -o parled12-lite daemon.c parport.c parports.c evloop.c client.c server.c slave.c config.c main.c
+gcc -std=c99 -Wpedantic -Wall -Wextra -D_DEFAULT_SOURCE -DLITE -fdata-sections -ffunction-sections -Wl,--gc-sections -Wl,--print-gc-sections -Wl,-s -o parled12lite daemon.c parport.c parports.c evloop.c client.c server.c slave.c config.c main.c
